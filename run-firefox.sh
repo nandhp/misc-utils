b3376ebd35ac2fd5047473490a72d61d51797f95
nandhp <nandhp@gmail.com>
2016-03-24 16:00:16 -0500

    run-firefox: Check for any running Firefox binary

--- run-firefox.sh
+++ run-firefox.sh
@@ -3,7 +3,8 @@
 # run-firefox.sh - Run Firefox, but only if already running.
 #
 
-FIREFOX=$(type -P firefox)
+# $(type -P firefox) does not work if it is a wrapper script
+FIREFOX=firefox
 
 if pidof "$FIREFOX" >/dev/null || \
    zenity --question --title Firefox --text '<b><big>Firefox is not running</big></b>\n\nWould you like to start Firefox now?'; then
